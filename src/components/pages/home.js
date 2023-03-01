78a7a44ffc7e15343449c003baf504332e9fbb8e
bestmycode <bestmycode610@gmail.com>
2023-02-28 16:09:18 -0800

    update for home

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import SliderCarouselRedux from '../components/SliderCarouselRedux';
+import SliderCarouselSingleRedux from '../components/SliderCarouselSingleRedux';
+import FeatureBox from '../components/FeatureBox';
 import CarouselCollectionRedux from '../components/CarouselCollectionRedux';
-import CarouselNewRedux from '../components/CarouselNewRedux';
+import ColumnNewRedux from '../components/ColumnNewRedux';
 import AuthorListRedux from '../components/AuthorListRedux';
 import Footer from '../components/footer';
 import { createGlobalStyle } from 'styled-components';
+import Reveal from 'react-awesome-reveal';
+import { keyframes } from "@emotion/react";
+
+const fadeInUp = keyframes`
+  0% {
+    opacity: 0;
+    -webkit-transform: translateY(40px);
+    transform: translateY(40px);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+`;
+const inline = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+  .d-inline{
+    display: inline-block;
+   }
+`;
 
 const GlobalStyles = createGlobalStyle`
   header#myHeader.navbar .search #quick_search{
@@ -39,19 +66,85 @@ const GlobalStyles = createGlobalStyle`
 const homethree= () => (
   <div>
   <GlobalStyles />
-      <section className="jumbotron no-bg no-bottom">
-        <div className='container-fluid'>
-          <div className='row'>
-             <SliderCarouselRedux/>
+      <section className="jumbotron no-bg">
+        <div className='container'>
+          <div className='row align-items-center'>
+            <div className='col-lg-6'>
+                <div className="spacer-single"></div>
+                <Reveal className='onStep' keyframes={fadeInUp} delay={0} duration={600} triggerOnce>
+                <h6 className=""><span className="text-uppercase color">Gigaland Market</span></h6>
+                </Reveal>
+                <div className="spacer-10"></div>
+                <Reveal className='onStep' keyframes={fadeInUp} delay={300} duration={600} triggerOnce>
+                <h1 className="">Create, sell or collect digital items.</h1>
+                </Reveal>
+                <Reveal className='onStep' keyframes={fadeInUp} delay={600} duration={600} triggerOnce>
+                <p className=" lead">
+                Unit of data stored on a digital ledger, called a blockchain, that certifies a digital asset to be unique and therefore not interchangeable
+                </p>
+                </Reveal>
+                <div className="spacer-10"></div>
+                <Reveal className='onStep' keyframes={fadeInUp} delay={800} duration={900} triggerOnce>
+                <span onClick={()=> window.open("/#", "_self")} className="btn-main lead">Explore</span>
+                <div className="mb-sm-30"></div>
+                </Reveal>
+                <Reveal className='onStep d-inline' keyframes={inline} delay={900} duration={1200} triggerOnce>
+                <div className="row">
+                    <div className="spacer-single"></div>
+                    <div className="row">
+                            <div className="col-lg-4 col-md-6 col-sm-4 mb30">
+                                <div className="de_count text-left">
+                                    <h3><span>94215</span></h3>
+                                    <h5 className="id-color">Collectibles</h5>
+                                </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 col-sm-4 mb30">
+                                <div className="de_count text-left">
+                                    <h3><span>27</span>k</h3>
+                                    <h5 className="id-color">Auctions</h5>
+                                </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 col-sm-4 mb30">
+                                <div className="de_count text-left">
+                                    <h3><span>4</span>k</h3>
+                                    <h5 className="id-color">NFT Artist</h5>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                </Reveal>
+                <div className="spacer-double"></div>
+            </div>
+             <div className='col-lg-6 px-0'>
+               <SliderCarouselSingleRedux/>
+             </div>
           </div>
         </div>
       </section>
 
-      <section className='container no-bottom'>
+      <section className='container no-top'>
+        <div className='container px-0'>
+          <FeatureBox/>
+        </div>
+      </section>
+
+      <section className='container'>
+      <div className='container'>
         <div className='row'>
-          <div className='col-lg-12 text-center'>
+          <div className='col-lg-12'>
+              <h2 className='style-2'>New Items</h2>
+          </div>
+        </div>
+       <ColumnNewRedux/>
+      </div>
+      </section>
+
+      <section className='container no-top'>
+        <div className='row'>
+          <div className='col-lg-12'>
               <h2 className='style-2'>Hot Collections</h2>
-              <div className="small-border"></div>
           </div>
         </div>
         <div className='container no-top'>
@@ -63,21 +156,10 @@ const homethree= () => (
         </div>
       </section>
 
-      <section className='container'>
-        <div className='row'>
-          <div className='col-lg-12 text-center'>
-              <h2 className='style-2'>New Items</h2>
-              <div className="small-border"></div>
-          </div>
-        </div>
-       <CarouselNewRedux/>
-      </section>
-
       <section className='container no-top'>
         <div className='row'>
-          <div className='col-lg-12 text-center'>
+          <div className='col-lg-12'>
               <h2 className='style-2'>Top Seller</h2>
-              <div className="small-border"></div>
           </div>
           <div className='col-lg-12'>
             <AuthorListRedux/>
@@ -85,7 +167,7 @@ const homethree= () => (
         </div>
       </section>
 
-      <section className='container no-top'>
+    <section className='container no-top'>
         <div className='row'>
           <div className='col-lg-12 text-center'>
               <h2 className='style-2'>Browse Category</h2>
