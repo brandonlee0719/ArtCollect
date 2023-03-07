64976d74d9c91ab9f13cca7300132b11a35234fd
bestmycode <bestmycode610@gmail.com>
2023-03-07 07:06:59 -0800

    update api

--- src/core/api.js
+++ src/core/api.js
@@ -1,5 +1,5 @@
 const api = {
-    baseUrl: 'http://localhost:1337',
+    baseUrl: 'https://artcollect.io',
     nfts: '/api/nfts',
     nftShowcases: '/api/nft_showcases',
     authors: '/api/authors',
